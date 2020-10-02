f1b8dadcf38a7dc702360926f166820e49aecbf2
Shu <stbook.syu@icloud.com>
2020-10-01 20:33:59 -0700

    node-mern

---
+++ models/user.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  dispalyName: {
+    type: String
+  }
+});
+
+module.exports = mongoose.model("User", userSchema);
---
+++ routes/user.route.js
@@ -0,0 +1,94 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.model");
+const auth = require("../middlewares/auth");
+
+router.post("/register", async (req, res) => {
+  try {
+    let { email, password, confirmPassword, displayName } = req.body;
+
+    if (!email || !password || !confirmPassword) {
+      return res.status(400).json({ msg: "Not all fields have been entered" });
+    }
+    if (password.length < 5) {
+      return res
+        .status(400)
+        .json({ msg: "The password needs to be at least 5 characters long." });
+    }
+    if (password !== confirmPassword) {
+      return res.status(400).json({ msg: "Passwords do not match." });
+    }
+
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ msg: "An account with this email already exists." });
+    }
+
+    if (!displayName) displayName = email;
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      email: email,
+      password: passwordHash,
+      displayName: displayName
+    });
+
+    const savedUser = await newUser.save();
+    res.json({ msg: "Created new user", savedUser });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ msg: "Not all fields have been entered." });
+    }
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ msg: "No account with this email has been registered." });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.json({
+      token: token,
+      user: {
+        id: user._id,
+        displayName: user.displayName
+      }
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.delete("/delete", auth, async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.user);
+    res.json(deletedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.get("/", async (req, res) => {
+  res.json({
+    msg: "Hello Node"
+  });
+});
+
+module.exports = router;
